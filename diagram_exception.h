7582db1e51ec72aa6fcf0efc7a7ace965af2688d
Nikita Maslov <nikitamslv@yandex.ru>
2017-05-09 23:04:37 +0300

    modified:   diagram_exception.h

--- diagram_exception.h
+++ diagram_exception.h
@@ -1,4 +1,32 @@
 #ifndef DIAGRAM_EXCEPTION_H
 #define DIAGRAM_EXCEPTION_H
 
+#include <iostream>
+namespace da {
+class InvalidDiagramException: public std::exception
+{
+public:
+    InvalidDiagramException() {}
+
+public:
+    const char *what() const throw()
+    {
+        return "structure of diagram is invalid";
+    }
+};
+
+class UnexpectedBlockException: public std::exception
+{
+public:
+    UnexpectedBlockException() {}
+
+public:
+    const char *what() const throw()
+    {
+        return "connection between parts of diagram is incorrect";
+    }
+};
+}
+
+
 #endif // DIAGRAM_EXCEPTION_H
